fe84060523317c9b3c98ebfc365f2624cf978c67
Rodrigo Uroz <rodrigouroz@gmail.com>
2024-10-08 20:19:22 +0000

    add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://3003e1112e89e653a72926bb820a26d8@o4507979775410176.ingest.us.sentry.io/4508088959434752",
+  integrations: [nodeProfilingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+
+  enabled: process.env.NODE_ENV !== "development",
+});
+
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import router from "./routes";
